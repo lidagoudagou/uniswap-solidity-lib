0907062a22760c9dc33a25b041d0bfdcd1dc0986
lidagoudagou <843962249@qq.com>
2022-12-10 23:21:09 +0800

    safeTransfer

--- contracts/libraries/TransferHelper.sol
+++ contracts/libraries/TransferHelper.sol
@@ -3,6 +3,7 @@
 pragma solidity >=0.6.0;
 
 // helper methods for interacting with ERC20 tokens and sending ETH that do not consistently return true/false
+// library相比较于contract，library内部的this指向于调用这个library的contract
 library TransferHelper {
     function safeApprove(
         address token,
@@ -23,6 +24,8 @@ library TransferHelper {
         uint256 value
     ) internal {
         // bytes4(keccak256(bytes('transfer(address,uint256)')));
+        // 0xa9059cbb 是方法 transfer(address,uint256)对应的签名码
+        // 这里调用的是对应token的合约的transfer方法。从sender账户发送value个币到to账户
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0xa9059cbb, to, value));
         require(
             success && (data.length == 0 || abi.decode(data, (bool))),
@@ -30,6 +33,7 @@ library TransferHelper {
         );
     }
 
+    // 代理转账，a地址可以帮b地址给c地址转账
     function safeTransferFrom(
         address token,
         address from,
@@ -37,6 +41,7 @@ library TransferHelper {
         uint256 value
     ) internal {
         // bytes4(keccak256(bytes('transferFrom(address,address,uint256)')));
+        // 前提是from账户给sender.address授权过该币
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x23b872dd, from, to, value));
         require(
             success && (data.length == 0 || abi.decode(data, (bool))),
@@ -44,6 +49,7 @@ library TransferHelper {
         );
     }
 
+    // ETH的交易方式不同，ETH与ERC20标准代币不一样
     function safeTransferETH(address to, uint256 value) internal {
         (bool success, ) = to.call{value: value}(new bytes(0));
         require(success, 'TransferHelper::safeTransferETH: ETH transfer failed');
